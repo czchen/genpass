e99cbcc58264d2bee5711f8caf0185e8378efed9
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-10-11 19:29:34 +0800

    Add test case

--- genpass.py
+++ genpass.py
@@ -6,6 +6,9 @@ def genpass(password, *, nonce='', salt='', iterations=0x10000):
     '''
     >>> genpass('')
     '9G39QAZXnhIqc7y.3gF5fMbbG752/tve'
+    >>> genpass('password', nonce='nonce', salt='salt', iterations=1)
+    'ztWpkVwEFEmoWkX.7Vk6e5oFkVgFPTsh'
+
     '''
     result = pbkdf2.crypt('{}{}'.format(password, nonce),
                           salt=salt, iterations=iterations)
